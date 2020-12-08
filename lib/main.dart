6a5baf57290fc83f8308a2220bfad19ef79da1a0
DeveloperAjit <indiandeveloperajit@gmail.com>
2020-12-08 11:25:17 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:management/Authentication/auth.dart';
-import 'package:management/Navigations/graph1.dart';
 import 'package:management/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:management/screens/login.dart';
